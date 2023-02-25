24e53a981831f23bf92295c5dedcf96c26723d12
Wesselfr <wessel.frijters@gmail.com>
2023-02-25 14:36:03 +0100

    Added some test documentation

--- inferno_engine/src/lib.rs
+++ inferno_engine/src/lib.rs
@@ -1,6 +1,16 @@
+//! # Inferno Engine
+//!
+//! Easy to use, rapid prototype engine written in Rust!
 pub mod reload;
 pub mod window;
 
+
+/// Draw an object using it's handle.
+/// # Example
+///
+/// ```
+/// engine_draw(0);
+/// ```
 pub fn engine_draw(handle: u32) {
     println!("Drawing resource: {}", handle);
 }
